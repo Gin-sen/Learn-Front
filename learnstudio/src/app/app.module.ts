d58452a257702d503c4a649b67a9afadb8e5142b
marine <bard1.mrn@gmail.com>
2021-05-26 09:50:33 +0200

    landing page and start auth

--- learnstudio/src/app/app.module.ts
+++ learnstudio/src/app/app.module.ts
@@ -5,16 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { RegisterComponent } from './pages/auth_component/register/register.component';
+import { LoginComponent } from './pages/auth_component/login/login.component';
+import { ClickOutsideModule } from 'ng-click-outside';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    ErrorPageComponent
+    ErrorPageComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClickOutsideModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
