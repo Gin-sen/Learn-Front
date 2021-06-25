cb2f23ebfe0d490ec14b62f251cc7032f08eb0bc
Canardier <CuckOrDuck@hotmail.com>
2021-06-25 17:17:48 +0200

    add modification at exercice

--- learnstudio/src/app/pages/logged-content/office-atmosphere/office-atmosphere.component.ts
+++ learnstudio/src/app/pages/logged-content/office-atmosphere/office-atmosphere.component.ts
@@ -11,24 +11,27 @@ export class OfficeAtmosphereComponent implements OnInit {
   constructor() { }
 
   play_pause = () => {
-    var audio = document.getElementById("audio");
+    var audio = new Audio();
+    audio = document.getElementById("audio");
     var pp_button = document.getElementsByClassName("pp_button")[0];
 
-    if (audio.paused) {
+    if (audio != null && audio.paused) {
       audio.play();
       pp_button.innerHTML = "Mettre fin au test";
     }
     else {
       audio.pause()
-      this.display = "none";
       var seconde = parseInt(audio.currentTime % 60);
       var minute = parseInt((audio.currentTime / 60) % 60);
+      var my_form = document.getElementsByClassName("my_form")[0];
+      console.log(my_form);
       alert("Vous avez écouter le bruit pendant " + minute + ":" + seconde + "  Félicitation !");
       audio.currentTime = 0;
-      pp_button.innerHTML = "Relancer le test";
+      pp_button.style.display = "none";
+      my_form.style.display = "block";
     }
     audio.onended = function() {
-        alert("La vidéo c'est terminé Félicitation !");
+        alert("Vous êtes arrivé au bout de l'exercice Félicitation !");
     }; 
   }
 
