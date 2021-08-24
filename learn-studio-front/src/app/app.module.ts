e30ac32faa1f1d976507b98dbc0e7c029c9a6f2b
marine <bard1.mrn@gmail.com>
2021-08-25 00:44:26 +0200

    component generation

--- learn-studio-front/src/app/app.module.ts
+++ learn-studio-front/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { AuthentificationComponentComponent } from './pages/authentification-component/authentification-component.component';
+import { LoginComponent } from './pages/authentification/login/login.component';
+import { RegisterComponent } from './pages/authentification/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent,
+    LandingPageComponent,
+    AuthentificationComponentComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
